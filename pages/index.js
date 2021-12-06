b195984f46db14d9700295e767847e795500af25
Rudy <rudy.quinternet@gmail.com>
2021-12-06 20:48:56 +0100

    init pages and a little bit of content (learning how to use Next)

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
 
 export default function Home() {
   return (
@@ -12,44 +14,38 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <h1 className={styles.main}>
+          Welcome to BaeForm !
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <Link href="/pages/q?v=1">
+            <a className={styles.card}>
+              <h2>Question &rarr;</h2>
+              <p>How are you?</p>
+            </a>
+          </Link>
+
+          <Link href="/pages/u?v=1">
+            <a className={styles.card}>
+              <h2>User &rarr;</h2>
+              <p>Rudy Quinternet</p>
+            </a>
+          </Link>
+
+          <Link href="/pages/c?v=1">
+            <a className={styles.card}>
+              <h2>Chat &rarr;</h2>
+              <p>Rudy + Marianne</p>
+            </a>
+          </Link>
+
+        </div>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer className={styles.footer}>
---
+++ pages/q.js
@@ -0,0 +1,12 @@
+export default function Question() {
+  return (
+    <div id="questionHolder">
+
+      <div className="answer"></div>
+      <div className="answer"></div>
+      <div className="answer"></div>
+      <div className="answer"></div>
+
+    </div>
+  )
+}

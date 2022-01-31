da2020cab7ebc53d69338005061788a649e8c10a
Rudy <rudy.quinternet@gmail.com>
2022-01-31 18:27:33 +0100

    css signup signin

--- components/LoginRegisterForm.js
+++ components/LoginRegisterForm.js
@@ -5,58 +5,75 @@ export default function LoginRegisterForm() {
   return (
     <div>
 
-<div id="formContainer" className="formContainer">
+      <div id="formContainer" className="formContainer">
 
-<form action="/api/u">
-  <div className="form-group">
-    <label htmlFor="InputFirstName">First Name</label>
-    <input type="text" className="form-control" id="InputFirstName" aria-describedby="emailHelp" placeholder="Rudy" />
-    <label htmlFor="InputLastName">Last Name</label>
-    <input type="text" className="form-control" id="InputLastName" aria-describedby="emailHelp" placeholder="Quinternet" />
-    <label htmlFor="birthday">Birthday:</label>
-    <input type="date" id="birthday" name="birthday">
-    </input>
-  </div>
-  <div className="form-group">
-    <label htmlFor="InputRegisterEmail">Email address</label>
-    <input type="email" className="form-control" id="InputRegisterEmail" aria-describedby="emailHelp" placeholder="Enter email" />
-    <small id="emailHelp" className="form-text text-muted">(We'll never share your email with anyone else.)</small>
-  </div>
-  <div className="form-group">
-    <label htmlFor="InputRegisterPassword">Password</label>
-    <input type="password" className="form-control" id="InputRegisterPassword" placeholder="Password" />
-    <label htmlFor="InputRegisterPasswordVerify">Password Verification</label>
-    <input type="password" className="form-control" id="InputRegisterPasswordVerify" placeholder="Password" />
-  </div>
-  <button onClick={() => { }} className="btn btn-primary">Submit</button>
-</form>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<form>
-  <div className="form-group">
-    <label htmlFor="InputLoginEmail">Email address</label>
-    <input type="email" className="form-control" id="InputLoginEmail" aria-describedby="emailHelp" placeholder="Enter email" />
-    <small id="emailHelp" className="form-text text-muted">(We'll never share your email with anyone else.)</small>
-  </div>
-  <div className="form-group">
-    <label htmlFor="InputLoginPassword">Password</label>
-    <input type="password" className="form-control" id="InputLoginPassword" placeholder="Password" />
-  </div>
-  <div className="form-group form-check">
-    <input type="checkbox" className="form-check-input" id="Check" />
-    <label className="form-check-label" htmlFor="Check">Check me out</label>
-  </div>
-  <button onClick={() => { console.log("pipou") }} className="btn btn-primary">Submit</button>
-</form>
+        <div className={styles.signUpForm}>
+          <div className="form-group">
+            <div className={styles.formInput}>
+              <label htmlFor="InputFirstName">First Name : </label>
+              <input type="text" className="form-control" id="InputFirstName" aria-describedby="emailHelp" placeholder="Rudy" />
+            </div>
+            <div className={styles.formInput}>
+              <label htmlFor="InputLastName">Last Name : </label>
+              <input type="text" className="form-control" id="InputLastName" aria-describedby="emailHelp" placeholder="Quinternet" />
+            </div>
+            <div className={styles.formInput}>
+              <label htmlFor="birthday">Birthday : </label>
+              <input type="date" id="birthday" name="birthday" />
+            </div>
+          </div>
+          <div className="form-group">
+            <div className={styles.formInput}>
+              <label htmlFor="InputRegisterEmail">Email address : </label>
+              <input type="email" className="form-control" id="InputRegisterEmail" aria-describedby="emailHelp" placeholder="Enter email" />
+              <small id="emailHelp" className={styles.small}>(We'll never share your email with anyone else.)</small>
+            </div>
+          </div>
+          <div className="form-group">
+            <div className={styles.formInput}>
+              <label htmlFor="InputRegisterPassword">Password : </label>
+              <input type="password" className="form-control" id="InputRegisterPassword" placeholder="Password" />
+            </div>
+            <div className={styles.formInput}>
+              <label htmlFor="InputRegisterPasswordVerify">Password Verification : </label>
+              <input type="password" className="form-control" id="InputRegisterPasswordVerify" placeholder="Password" />
+            </div>
+          </div>
+          <button onClick={() => { }} className={styles.submitBtn}>Submit</button>
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <div className={styles.signInForm}>
+          <div className="form-group">
+            <div className={styles.formInput}>
+              <label htmlFor="InputLoginEmail">Email address : </label>
+              <input type="email" className="form-control" id="InputLoginEmail" aria-describedby="emailHelp" placeholder="Enter email" />
+              <small id="emailHelp" className={styles.small}>(We'll never share your email with anyone else.)</small>
+            </div>
+          </div>
+          <div className="form-group">
+            <div className={styles.formInput}>
+              <label htmlFor="InputLoginPassword">Password : </label>
+              <input type="password" className="form-control" id="InputLoginPassword" placeholder="Password" />
+            </div>
+          </div>
+          <div className="form-group form-check">
+            <div className={styles.formInput}>
+              <p className={styles.formCheckP} htmlFor="Check">Remember me?</p>
+              <input type="checkbox" className={styles.formCheckInput} id="Check" />
+            </div>
+          </div>
+          <button onClick={() => { console.log("pipou") }} className={styles.submitBtn}>Submit</button>
+        </div>
 
-<br></br>
-<button onClick={() => { }}>Password Forgot</button>
-<button onClick={() => { flip("red"); }}>Create account</button>
-<button onClick={() => { flip("red"); }}>I already have an account</button>
+        <br></br>
+        <button onClick={() => { }}>Password Forgot</button>
+        <button onClick={() => { flip("red"); }}>Create account</button>
+        <button onClick={() => { flip("red"); }}>I already have an account</button>
 
-</div>
-    </div>
+      </div>
+    </div >
   )
 }

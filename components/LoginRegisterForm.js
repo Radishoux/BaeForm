43d3a9281c61be3d9db62f8e751af375bad335da
Rudy <rudy.quinternet@gmail.com>
2022-02-14 00:28:07 +0100

    token + signin + signup

--- components/LoginRegisterForm.js
+++ components/LoginRegisterForm.js
@@ -2,76 +2,144 @@ import styles from './LoginRegisterForm.module.css'
 import React from 'react'
 
 export default function LoginRegisterForm() {
+
+  function hideform(formId) {
+    document.getElementById(formId).className = styles.hiddenform;
+    setTimeout(() => {
+      document.getElementById(formId).style.display = 'none';
+    }, 1000);
+  }
+
+  function showform(formId) {
+    document.getElementById(formId).style.display = 'block';
+    setTimeout(() => {
+      document.getElementById(formId).className = styles.shownform;
+    }, 100);
+  }
+
+  function isEmail(str) {
+    if (str.indexOf('@') == -1) {
+      return false;
+    }
+    if (str.indexOf('.') == -1) {
+      return false;
+    }
+    return true;
+  }
+
+  function signIn() {
+    if (!isEmail(document.getElementById("InputLoginEmail").value)) {
+      alert("Please enter a valid email address");
+      return;
+    }
+
+    fetch('/api/u', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "email": document.getElementById("InputRegisterEmail").value,
+        "password": document.getElementById("InputRegisterPassword").value
+      })
+    })
+      .then(postres => postres.text())
+      .then(postres => console.log(postres))
+  }
+
+  function signUp() {
+    if (document.getElementById("InputLastName").value.length < 1) {
+      alert("Please enter your last name");
+      return;
+    }
+
+    if (document.getElementById("InputFirstName").value.length < 1) {
+      alert("Please enter your first name");
+      return;
+    }
+
+    if (!isEmail(document.getElementById("InputRegisterEmail").value)) {
+      alert("Please enter a valid email address");
+      return;
+    }
+
+    if (document.getElementById("InputRegisterPassword").value && document.getElementById("InputRegisterPasswordConfirm").value && document.getElementById("InputRegisterPassword").value === document.getElementById("InputRegisterPasswordConfirm").value) {
+      fetch('/api/u', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          "email": document.getElementById("InputRegisterEmail").value,
+          "lastName": document.getElementById("InputLastName").value,
+          "firstName": document.getElementById("InputFirstName").value,
+          "password": document.getElementById("InputRegisterPassword").value
+        })
+      })
+        .then(postres => postres.text())
+        .then(postres => console.log(postres))
+    }
+    else {
+      alert("Passwords do not match")
+    }
+  }
+
   return (
     <div>
 
       <div id="formContainer" className="formContainer">
 
-        <div className={styles.signUpForm}>
+        <div id='signUpForm' className={styles.hiddenform} style={{display:"none"}}>
           <div className="form-group">
-            <div className={styles.formInput}>
-              <label htmlFor="InputFirstName">First Name : </label>
-              <input type="text" className="form-control" id="InputFirstName" aria-describedby="emailHelp" placeholder="Rudy" />
+            <div className={styles.formInputDiv}>
+              <label  className={styles.formLabel} htmlFor="InputFirstName">First Name : </label>
+              <input type="text" className={styles.formInput} id="InputFirstName" aria-describedby="emailHelp" placeholder="Rudy" />
             </div>
-            <div className={styles.formInput}>
-              <label htmlFor="InputLastName">Last Name : </label>
-              <input type="text" className="form-control" id="InputLastName" aria-describedby="emailHelp" placeholder="Quinternet" />
-            </div>
-            <div className={styles.formInput}>
-              <label htmlFor="birthday">Birthday : </label>
-              <input type="date" id="birthday" name="birthday" />
+            <div className={styles.formInputDiv}>
+              <label  className={styles.formLabel} htmlFor="InputLastName">Last Name : </label>
+              <input type="text" className={styles.formInput} id="InputLastName" aria-describedby="emailHelp" placeholder="Quinternet" />
             </div>
           </div>
           <div className="form-group">
-            <div className={styles.formInput}>
-              <label htmlFor="InputRegisterEmail">Email address : </label>
-              <input type="email" className="form-control" id="InputRegisterEmail" aria-describedby="emailHelp" placeholder="Enter email" />
+            <div className={styles.formInputDiv}>
+              <label  className={styles.formLabel} htmlFor="InputRegisterEmail">Email address : </label>
+              <input type="email" className={styles.formInput} id="InputRegisterEmail" aria-describedby="emailHelp" placeholder="email@email.com" />
               <small id="emailHelp" className={styles.small}>(We'll never share your email with anyone else.)</small>
             </div>
           </div>
           <div className="form-group">
-            <div className={styles.formInput}>
-              <label htmlFor="InputRegisterPassword">Password : </label>
-              <input type="password" className="form-control" id="InputRegisterPassword" placeholder="Password" />
+            <div className={styles.formInputDiv}>
+              <label  className={styles.formLabel} htmlFor="InputRegisterPassword">Password : </label>
+              <input type="password" className={styles.formInput} id="InputRegisterPassword" placeholder="Password" />
             </div>
-            <div className={styles.formInput}>
-              <label htmlFor="InputRegisterPasswordVerify">Password Verification : </label>
-              <input type="password" className="form-control" id="InputRegisterPasswordVerify" placeholder="Password" />
+            <div className={styles.formInputDiv}>
+              <label  className={styles.formLabel} htmlFor="InputRegisterPasswordConfirm">Password Verification : </label>
+              <input type="password" className={styles.formInput} id="InputRegisterPasswordConfirm" placeholder="Password" />
             </div>
           </div>
-          <button onClick={() => { }} className={styles.submitBtn}>Submit</button>
+          <button onClick={() => { signUp() }} className={styles.submitBtn}>Sign-up</button>
+          <button className={styles.submitBtn} onClick={() => { hideform("signUpForm"); setTimeout(() => {showform("signInForm")}, 1000);}}>I already have an account</button>
         </div>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <div className={styles.signInForm}>
+        <div id='signInForm' className={styles.shownform}>
           <div className="form-group">
-            <div className={styles.formInput}>
-              <label htmlFor="InputLoginEmail">Email address : </label>
-              <input type="email" className="form-control" id="InputLoginEmail" aria-describedby="emailHelp" placeholder="Enter email" />
+            <div className={styles.formInputDiv}>
+              <label  className={styles.formLabel} htmlFor="InputLoginEmail">Email address : </label>
+              <input type="email" className={styles.formInput} id="InputLoginEmail" aria-describedby="emailHelp" placeholder="email@email.com" />
               <small id="emailHelp" className={styles.small}>(We'll never share your email with anyone else.)</small>
             </div>
           </div>
           <div className="form-group">
-            <div className={styles.formInput}>
-              <label htmlFor="InputLoginPassword">Password : </label>
-              <input type="password" className="form-control" id="InputLoginPassword" placeholder="Password" />
+            <div className={styles.formInputDiv}>
+              <label  className={styles.formLabel} htmlFor="InputLoginPassword">Password : </label>
+              <input type="password" className={styles.formInput} id="InputLoginPassword" placeholder="Password" />
             </div>
           </div>
-          <div className="form-group form-check">
-            <div className={styles.formInput}>
-              <p className={styles.formCheckP} htmlFor="Check">Remember me?</p>
-              <input type="checkbox" className={styles.formCheckInput} id="Check" />
-            </div>
-          </div>
-          <button onClick={() => { console.log("pipou") }} className={styles.submitBtn}>Submit</button>
+          <button onClick={() => { signIn() }} className={styles.submitBtn}>Sign-in</button>
+          {/* <button className={styles.submitBtn} onClick={() => { }}>Password Forgot</button> */}
+          <button className={styles.submitBtn} onClick={() => { hideform("signInForm"); setTimeout(() => {showform("signUpForm")}, 1000);}}>Create account</button>
         </div>
 
         <br></br>
-        <button onClick={() => { }}>Password Forgot</button>
-        <button onClick={() => { flip("red"); }}>Create account</button>
-        <button onClick={() => { flip("red"); }}>I already have an account</button>
 
       </div>
     </div >
--- pages/api/u.js
+++ pages/api/u.js
@@ -1,4 +1,16 @@
-export default function handler(req, res) {
-    res.status(200).json(req.body)
-        // res.status(200).json(req.query)
+import axios from "axios";
+
+export default async function handler(req, res) {
+
+    if (req.method === "PUT") {
+        const respp = await axios.put('https://ovfdgc9m1k.execute-api.eu-west-3.amazonaws.com/items', req.body)
+        res.status(200).json(respp.data)
+    } else if (req.method === "POST") {
+        const respp = await axios.post('https://ovfdgc9m1k.execute-api.eu-west-3.amazonaws.com/items', req.body)
+        res.status(200).json(respp.data)
+    } else {
+        res.status(200).json("wrong method");
+    }
+    // console.log(req.body)
+    // console.log(req.headers)
 }
